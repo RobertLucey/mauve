cb07f1f2bc2794b597d8ce8e2c7b457383f85d93
Robert Lucey <robert@glimpse.ie>
2021-01-18 17:13:43 +0000

    Test push to splunk

--- src/mauve/bin/push_to_splunk.py
+++ src/mauve/bin/push_to_splunk.py
@@ -13,13 +13,31 @@ from mauve.constants import (
 )
 
 
-def get_tokens(b):
+def push_to_splunk(b):
     try:
         b.push_to_splunk()
         del b
     except Exception as ex:
         print('BAD: %s %s' (b.title, ex))
         pass
+
+
+def process_files(num_processes=4):
+    files = []
+    for i in iter_books(source='local_text'):
+        if os.path.exists(i.content_path + '.tokenv{}.pickle'.format(TOKEN_VERSION)):
+            files.append(i)
+
+    if num_processes == 1:
+        for f in files:
+            push_to_splunk(f)
+    else:
+        pool = Pool(processes=num_processes)
+        for _ in tqdm.tqdm(
+            pool.imap_unordered(push_to_splunk, files),
+            total=len(files)
+        ):
+            pass
 
 
 def main():
@@ -29,21 +47,12 @@ def main():
         '--processes',
         type=int,
         dest='num_processes',
-        default=6
+        default=4
     )
     args = parser.parse_args()
 
-    books = []
-    for i in iter_books(source='local_text'):
-        if os.path.exists(i.content_path + '.tokenv{}.pickle'.format(TOKEN_VERSION)):
-            books.append(i)
+    process_files(num_processes=args.num_processes)
 
-    pool = Pool(processes=args.num_processes)
-    for _ in tqdm.tqdm(
-        pool.imap_unordered(get_tokens, books),
-        total=len(books)
-    ):
-        pass
 
 if __name__ == '__main__':
     main()
