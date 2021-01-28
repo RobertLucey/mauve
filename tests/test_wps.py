4f680775c07bea4d1b8737b3861aba879416d87b
Robert Lucey <robert@glimpse.ie>
2021-01-28 01:41:23 +0000

    Move test wps

---
+++ tests/test_wps.py
@@ -0,0 +1,26 @@
+from time import sleep
+from unittest import TestCase
+
+from mauve.wps import WPS
+
+
+class TestWPS(TestCase):
+
+    def test_wps(self):
+        wps = WPS()
+        for _ in range(100):
+            sleep(0.05)
+            wps.update()
+        self.assertTrue(abs(wps.wps() - 20.0) < 0.2)
+
+        wps = WPS(past_seconds=1)
+        for _ in range(30):
+            sleep(0.05)
+            wps.update()
+        self.assertTrue(len(wps.data) == 20)
+
+        wps = WPS(past_seconds=1, print_rate=1)
+        for _ in range(30):
+            sleep(0.05)
+            wps.update()
+        self.assertTrue(len(wps.data) == 20)
