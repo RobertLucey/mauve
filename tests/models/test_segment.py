1473f9cd2b3dcd2ef29cc7d3dea7e3659294e2af
Robert Lucey <robert@glimpse.ie>
2021-02-10 23:09:17 +0000

    Some more segment tests

--- tests/models/test_segment.py
+++ tests/models/test_segment.py
@@ -4,7 +4,7 @@ import mock
 import glob
 import os
 
-from mauve.models.segment import Segment
+from mauve.models.segment import Segment, Segments
 from mauve.models.sentence import Sentence
 from mauve.models.books.book import Book
 from mauve.models.books.review import Reviews, Review
@@ -13,6 +13,19 @@ from mauve import constants
 
 from mauve.models.synonym import Synonym
 
+
+class TestSegments(TestCase):
+
+    def test_vanity(self):
+        segments = Segments()
+        segments.extend([Segment('The'), Segment('end')])
+        segments.append(Segment('is'))
+        segments.append(Segment('nigh'))
+
+        self.assertEqual(
+            ' '.join([s.text for s in segments]),
+            'The stop is nigh'
+        )
 
 class TestSegment(TestCase):
 
@@ -30,6 +43,30 @@ class TestSegment(TestCase):
             'dunno'
         )
 
+    def test_is_adj(self):
+        self.assertTrue(
+            Segment('blue').is_adj
+        )
+        self.assertFalse(
+            Segment('house').is_adj
+        )
+
+    def test_is_adv(self):
+        self.assertTrue(
+            Segment('very').is_adv
+        )
+        self.assertFalse(
+            Segment('trumpet').is_adv
+        )
+
+    def test_is_verb(self):
+        self.assertTrue(
+            Segment('looking').is_verb
+        )
+        self.assertFalse(
+            Segment('blue').is_verb
+        )
+        
     def test_is_entity(self):
         self.assertTrue(Segment('asd.', tag='DATE').is_entity)
         self.assertFalse(Segment('asd.', tag='NN').is_entity)
