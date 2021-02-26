5cf9a404e0108b6ce475bfdf193e81608fefbfb9
Robert Lucey <robert@glimpse.ie>
2021-02-26 19:03:24 +0000

    Add some person stuff tests

--- tests/models/test_person.py
+++ tests/models/test_person.py
@@ -4,7 +4,10 @@ import mock
 import glob
 import os
 
-from mauve.models.person import Person
+from mauve.models.person import (
+    Person,
+    People
+)
 
 
 class TestPerson(TestCase):
@@ -39,3 +42,61 @@ class TestPerson(TestCase):
                 gender,
                 name
             )
+
+    def test_is_trustworthy(self):
+        person = Person(name='a', trustworthy=False)
+        self.assertFalse(person.is_trustworthy)
+
+        person = Person(name='a', trustworthy=False)
+        for i in range(100):
+            person.inc_references()
+        self.assertTrue(person.is_trustworthy)
+
+        person = Person(name='a', trustworthy=True)
+        self.assertTrue(person.is_trustworthy)
+
+    def test_is_similar_to(self):
+        self.assertTrue(
+            Person(name='bOB').is_similar_to(Person(name='Bob'))
+        )
+        self.assertTrue(
+            Person(name='Mr Man').is_similar_to(Person(name='Man'))
+        )
+        self.assertTrue(
+            Person(name='a Something').is_similar_to(Person(name='Something'))
+        )
+        self.assertFalse(
+            Person(name='Blahlala').is_similar_to(Person(name='Blaaaaaaaa'))
+        )
+
+    def test_references(self):
+        person = Person(name='a')
+        self.assertEqual(person.references, 0)
+
+        person.inc_references()
+        self.assertEqual(person.references, 1)
+
+
+class TestPeople(TestCase):
+
+    def test_append(self):
+        people = People()
+        people.append(Person(name='a'))
+        self.assertEqual(people[0].references, 1)
+        people.append(Person(name='a'))
+        self.assertEqual(people[0].references, 2)
+
+    def test_remove_near_duplicates(self):
+        people = People()
+        people.extend([
+            Person(name='Mr Man'),
+            Person(name='Man'),
+            Person(name='Man'),
+        ])
+
+        people.remove_near_duplicates()
+
+        self.assertEqual(len(people), 1)
+        self.assertEqual(people[0].name, 'Man')
+        self.assertEqual(people[0].references, 3)
+
