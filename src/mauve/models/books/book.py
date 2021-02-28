bd13e3ac8785c1e4bc611cddc0529e0114dd3ffe
Robert Lucey <robert@glimpse.ie>
2021-02-28 23:27:52 +0000

    Remove some prints

--- src/mauve/models/books/book.py
+++ src/mauve/models/books/book.py
@@ -203,7 +203,6 @@ class Book(TextBody):
             return False
 
         if not self.author_similarity:
-            print('SIMILARITY BAD')
             # Likely not the same author
             return False
 
@@ -212,8 +211,6 @@ class Book(TextBody):
     @property
     def author_similarity(self):
         filename_author = os.path.basename(self.content_path).split('___')[1].replace('.', '').lower()
-        print(self.author.name)
-        print(filename_author)
         return self.author.is_similar_to(Person(name=filename_author))
 
     @property
