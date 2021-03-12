2d741242b7883564781633ae660fa5e000d71e1f
Robert Lucey <robert@glimpse.ie>
2021-03-12 22:53:37 +0000

    Move tagger
    Wah

--- src/mauve/__init__.py
+++ src/mauve/__init__.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import random
 
 import spacy
 
@@ -51,3 +52,25 @@ except:  # pragma: nocover
 
 WPS = WPS(print_rate=10000)
 SYNONYM = Synonym()
+
+from nltk.tag.perceptron import PerceptronTagger
+
+
+TAGGER = PerceptronTagger()
+
+
+class Tagger():
+
+    def pos_tag(self, tokens):
+        if tokens == ['']:
+            return []
+
+        tagged_tokens = TAGGER.tag(tokens)
+
+        if random.random() < 0.5:
+            count = 0
+            for tok, tag in tagged_tokens:
+                if count % 2 == 0:
+                    TAGGER.tagdict[tok] = tag
+
+        return tagged_tokens
