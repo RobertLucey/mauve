035f8a3191ee9b3aa6be6a9e534bc4395a3e2eb7
Robert Lucey <robert@glimpse.ie>
2021-03-20 18:00:07 +0000

    Add some more entry points to bin scripts

--- setup.py
+++ setup.py
@@ -35,13 +35,17 @@ setup(
     install_requires=INSTALL_REQUIRES,
     entry_points={
         'console_scripts': [
-            'mauve_convert_filenames = mauve.bin.convert_filenames:main',
-            'mauve_load_tokens = mauve.bin.load_tokens:main',
-            'mauve_epub_slimmer = mauve.bin.rm_images:main',
+            'mauve_scrape_oireachtas = mauve.bin.scrape_oireachtas:main',
             'mauve_scrape_calibre = mauve.bin.scrape_calibre:main',
             'mauve_scrape_eye = mauve.bin.scrape_eye:main',
             'mauve_scrape_goodreads = mauve.bin.scrape_goodreads:main',
+            'mauve_scrape_author_nationality = mauve.bin.scrape_author_nationality:main',
+            'mauve_convert_filenames = mauve.bin.convert_filenames:main',
+            'mauve_load_tokens = mauve.bin.load_tokens:main',
+            'mauve_epub_slimmer = mauve.bin.rm_images:main',
+            'mauve_mobi_to_epub = mauve.bin.mobi_to_epub:main',
             'mauve_epub_to_text = mauve.bin.to_text:main',
+            'mauve_compress_files = mauve.bin.compress_files:main',
         ]
     }
 )
