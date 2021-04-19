bad9ae73111af060bd677246d393f83998d456a4
Robert Lucey <robert@glimpse.ie>
2021-04-19 21:08:59 +0100

    Add some more failing tests

--- tests/test_number_convert.py
+++ tests/test_number_convert.py
@@ -15,9 +15,13 @@ class TestNumberConvert(TestCase):
 
     def test_number_convert_only_numbers_at_end(self):
         self.assertEqual(convert_numbers('something zero'), 'something 0')
+        self.assertEqual(convert_numbers('something fifty-three'), 'something 53')
+        self.assertEqual(convert_numbers('something one million and fifty-three'), 'something 1000053')
 
     def test_number_convert_only_numbers_at_start(self):
         self.assertEqual(convert_numbers('zero something'), ' 0  something')
+        self.assertEqual(convert_numbers('fifty-three something'), ' 53  something')
+        self.assertEqual(convert_numbers('one million and fifty-three something'), '1000053 something')
 
     def test_number_convert_numbers_in_middle(self):
         self.assertEqual(
